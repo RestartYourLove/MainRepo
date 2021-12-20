59b55ac3b6f99db5d3b0537e59d0ba4ec22c611d
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-12-20 20:15:06 +0200

    Hello World

---
+++ Final-Project-Restart-Your-Love/src/main/java/lv/restart/your/love/Final/Project/Restart/Your/Love/controller/HomePageController.java
@@ -0,0 +1,16 @@
+package lv.restart.your.love.Final.Project.Restart.Your.Love.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomePageController {
+
+    @GetMapping(value = "/home")
+    public String getHomePage(Model model) {
+        //model.addAttribute("myName", "Alexandra Gorbule");
+        return "home.html";
+    }
+}
