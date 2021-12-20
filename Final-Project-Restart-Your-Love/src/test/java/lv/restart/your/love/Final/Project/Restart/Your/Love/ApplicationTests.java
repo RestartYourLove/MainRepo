7906a6415a5c3b3b64d206927fb9553e5171d4e2
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-12-20 19:06:21 +0200

    First Commit

---
+++ Final-Project-Restart-Your-Love/src/test/java/lv/restart/your/love/Final/Project/Restart/Your/Love/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.restart.your.love.Final.Project.Restart.Your.Love;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
