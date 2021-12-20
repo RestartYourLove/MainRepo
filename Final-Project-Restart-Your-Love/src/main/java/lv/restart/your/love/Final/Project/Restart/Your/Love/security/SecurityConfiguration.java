4cd89179f2cdf6bd1c5f262af91b396a9efc1cf3
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-12-20 21:35:57 +0200

    + .antMatchers("/home").permitAll()

--- Final-Project-Restart-Your-Love/src/main/java/lv/restart/your/love/Final/Project/Restart/Your/Love/security/SecurityConfiguration.java
+++ Final-Project-Restart-Your-Love/src/main/java/lv/restart/your/love/Final/Project/Restart/Your/Love/security/SecurityConfiguration.java
@@ -9,7 +9,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
+                .antMatchers("/home").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
