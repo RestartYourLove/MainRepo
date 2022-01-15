591c37918a4451ded810feb800dbe1f416d1e119
kindofliz <simanovicha@gmail.com>
2022-01-15 12:12:43 +0200

    jasypt encrypt

--- FinalProjectRestartYourLove/src/main/java/lv/restart/your/love/Final/Project/Restart/Your/Love/Application.java
+++ FinalProjectRestartYourLove/src/main/java/lv/restart/your/love/Final/Project/Restart/Your/Love/Application.java
@@ -1,9 +1,11 @@
 package lv.restart.your.love.Final.Project.Restart.Your.Love;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Application {
 
 	public static void main(String[] args) {
